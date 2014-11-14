474d1a7f6b36fa9fc469ad521f50d4d343cbef32
Jim Cheung <usr.jim@gmail.com>
2014-11-15 01:39:39 +0800

    init import

---
+++ init.el
@@ -0,0 +1,26 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+(load "usrj-func")
+(my-depen)
+(my-env)
+(php-setup)
+(go-setup)
+
+(setq default-directory "/opt")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
