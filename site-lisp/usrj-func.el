b1b663b0b081c5541051d9cd8cfb5eb29013bd8d
Jim Cheung <usr.jim@gmail.com>
2016-08-31 10:55:05 +0800

    update go-mode

--- site-lisp/usrj-func.el
+++ site-lisp/usrj-func.el
@@ -396,7 +396,7 @@
         (buffer-list)))
 
 (defun usrj/go-setup ()
-  (require 'go-mode-load)
+  (require 'go-mode-autoloads)
   (add-hook 'before-save-hook 'gofmt-before-save)
   (add-hook 'go-mode-hook '(lambda ()
                              (local-set-key (kbd "C-c C-i") 'go-import-add)
