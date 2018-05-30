a76eb451b6eeef49df5ad3fc12b7e77e3c2e3da4
Jim Cheung <usr.jim@gmail.com>
2018-05-30 12:08:53 +0800

    resolved conflict

--- site-lisp/usrj-eshell.el
+++ site-lisp/usrj-eshell.el
@@ -92,4 +92,4 @@ directory to make multiple eshell windows easier."
 (setq eshell-where-to-jump 'begin)
 (setq eshell-review-quick-commands nil)
 (setq eshell-smart-space-goes-to-end t)
-
+(setq tramp-ssh-controlmaster-options "")
