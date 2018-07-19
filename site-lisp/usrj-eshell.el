40dffa819c50d1a58ebbc4ee27fe0de9a43cfc45
Jim Cheung <usr.jim@gmail.com>
2018-07-19 15:21:58 +0800

    change ido to ivy

--- site-lisp/usrj-eshell.el
+++ site-lisp/usrj-eshell.el
@@ -89,7 +89,7 @@ directory to make multiple eshell windows easier."
 
 (setq eww-search-prefix "https://www.google.com/search?q=")
 
-(setq eshell-where-to-jump 'begin)
-(setq eshell-review-quick-commands nil)
+(setq eshell-where-to-jump 'end)
+(setq eshell-review-quick-commands t)
 (setq eshell-smart-space-goes-to-end t)
 (setq tramp-ssh-controlmaster-options "")
