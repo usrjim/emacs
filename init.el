f446769ffcdad44a136f877093f59995c06413ca
Jim Cheung <usr.jim@gmail.com>
2021-01-20 11:49:04 +0800

    update config

--- init.el
+++ init.el
@@ -2,6 +2,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; basic
 (set-language-environment "utf-8")
 
 (projectile-global-mode)
@@ -21,17 +22,26 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+;; keys
+(global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C->") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-c 8") 'isearch-forward-symbol-at-point)
+(global-set-key (kbd "C-=") 'er/expand-region)
 
 (define-prefix-command 'usrj-map)
 (global-set-key (kbd "C-,") 'usrj-map)
 (define-key usrj-map (kbd "a") 'embrace-commander)
 (define-key usrj-map (kbd "e") 'cider-eval-last-sexp)
 (define-key usrj-map (kbd "m") 'cider-eval-defun-to-comment)
+(define-key usrj-map (kbd "r") 'counsel-recentf)
 
+;; hooks
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+;; custom functions
 (defun usrj/md-link(url)
   (interactive (list(read-string "url: ")))
   (let (title buf)
@@ -47,5 +57,6 @@
   (interactive)
   (insert (format-time-string "%A, %B %d, %Y")))
 
+;; custom variables
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
